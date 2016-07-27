86507c99f2e5917b242f7e3d8daec9a55d904fb5
Pabitra Muni <mailtopabi@gmail.com>
2016-07-27 23:57:54 +0530

    framework setup

--- school-management-auth/src/main/java/org/ahant/auth/controller/AuthDataProcess.java
+++ school-management-auth/src/main/java/org/ahant/auth/controller/AuthDataProcess.java
@@ -1,14 +1,10 @@
 package org.ahant.auth.controller;
 
 import org.ahant.auth.dao.LoginDao;
-import org.ahant.auth.exception.InvalidCredentialException;
 import org.ahant.auth.model.User;
 import org.ahant.core.controller.DataProcessor;
 import org.ahant.core.exception.NotFoundException;
 import org.ahant.core.model.TaskData;
-
-import static org.ahant.auth.constants.LoginConstants.INVALID_CREDENTIAL;
-import static org.ahant.auth.constants.LoginConstants.USER_NOT_FOUND;
 
 /**
  * Created by ahant on 7/27/2016.
@@ -25,6 +21,9 @@ public class AuthDataProcess implements DataProcessor {
         } else {
             taskData.setTarget(userDetails);
         }
+    }
 
+    public void setLoginDao(LoginDao loginDao) {
+        this.loginDao = loginDao;
     }
 }
